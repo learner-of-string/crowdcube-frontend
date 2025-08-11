ff3d9bf8003611a8a1774dfee4955a65001f0a35
Learner of String <aref0171personal@gmail.com>
2025-08-11 19:31:35 +0600

    activated sign up with email password and others bullshit stuffs

--- src/Pages/auth/SignUp.jsx
+++ src/Pages/auth/SignUp.jsx
@@ -4,10 +4,50 @@ import googleSvg from "@/assets/google.svg";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "sonner";
+import { AuthContext } from "../../Context/AuthContext";
 
 const SignUp = () => {
+    const { signUpUser, setUser } = useContext(AuthContext);
+
     // TODO: implement sign up with email, password and other necessary stuffs
+
+    const signUpWithEmailPasswordAndOthers = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const password = form.password.value;
+        console.log(name, email, phone, password);
+
+        signUpUser(email, password)
+            .then((res) => {
+                console.log(res.user);
+                setUser(res?.user);
+                const newUser = { name, email, phone };
+                fetch(`${import.meta.env.VITE_serverLink}/users`, {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify(newUser),
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        console.log("user created to db ", data);
+                        toast("Signed up successfully!");
+                        form.reset();
+                    });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     // TODO: implement sign up with google pop up
 
     return (
@@ -17,7 +57,10 @@ const SignUp = () => {
                 <h1 className="text-5xl text-center font-semibold mt-16">
                     Sign Up Now!
                 </h1>
-                <form className="w-md mx-auto space-y-5 mt-10 pb-10 border-b border-green-800/50">
+                <form
+                    onSubmit={signUpWithEmailPasswordAndOthers}
+                    className="w-md mx-auto space-y-5 mt-10 pb-10 border-b border-green-800/50"
+                >
                     <div className="w-full">
                         <Label className="pl-4 mb-1" htmlFor="name">
                             Your name
@@ -29,18 +72,7 @@ const SignUp = () => {
                             name="name"
                         />
                     </div>
-                    <div className="w-full">
-                        <Label className="pl-4 mb-1" htmlFor="password">
-                            Contact (if possible provide your whatsapp number)
-                        </Label>
-                        <Input
-                            placeholder="Your phone number with country code e.g: 8801234567890"
-                            type="number"
-                            className="meow"
-                            required
-                            name="password"
-                        />
-                    </div>
+
                     <div className="w-full">
                         <Label className="pl-4 mb-1" htmlFor="email">
                             Email
@@ -50,6 +82,18 @@ const SignUp = () => {
                             type="email"
                             required
                             name="email"
+                        />
+                    </div>
+                    <div className="w-full">
+                        <Label className="pl-4 mb-1" htmlFor="password">
+                            Contact (if possible provide your whatsapp number)
+                        </Label>
+                        <Input
+                            placeholder="Your phone number with country code e.g: 8801234567890"
+                            type="number"
+                            className="meow"
+                            required
+                            name="phone"
                         />
                     </div>
                     <div className="w-full">
